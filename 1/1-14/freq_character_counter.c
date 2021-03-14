4c6bdf9d42163e019fad8d321a01a5225e6c30a2
Mrinal Chanda <71232428+mrinal-devc@users.noreply.github.com>
2021-03-14 22:33:01 +0530

    freq_character_counter
    With getchar() function

---
+++ 1/1-14/freq_character_counter.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+main()
+{
+    int c;
+    char freq[128];
+    for(int i=0;i<128;i++)
+    {
+        freq[i] = 0;
+    }
+    while((c = getchar()) != EOF)
+    {
+        if(c == '\n')
+        {
+            ;
+        }
+        else
+        {
+            freq[c]++;
+        }
+    }
+    for(int i = 0;i< 128;i++)
+    {
+        if(freq[i] != 0)
+        {
+            printf("\n%c = %d",i,freq[i]);
+        }
+    }
+
+}
